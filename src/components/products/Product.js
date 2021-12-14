2a8c3894bc14c499e4425d413781308db556bb9b
carlosbarondev <barondev@gmail.com>
2021-12-14 15:33:39 +0100

    Cards añadidas

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,5 +1,20 @@
+import { Card } from "react-bootstrap"
+
+
 export const Product = () => {
     return (
-        <h3>Product</h3>
+        <Card>
+            <Card.Img variant="top" src="https://m.media-amazon.com/images/I/31oEszTc67L.jpg" />
+            <Card.Body>
+                <Card.Title>Card title</Card.Title>
+                <Card.Text>
+                    This is a longer card with supporting text below as a natural
+                    lead-in to additional content. This content is a little bit longer.
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Last updated 3 mins ago</small>
+            </Card.Footer>
+        </Card>
     )
 }
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,7 +1,14 @@
+import { Col, Row } from "react-bootstrap"
 import { Product } from "./Product"
 
 export const Products = () => {
     return (
-        <Product />
+        <Row xs={2} sm={3} md={5} lg={8} xl={12} className="g-0">
+            {Array.from({ length: 18 }).map((_, idx) => (
+                <Col>
+                    <Product />
+                </Col>
+            ))}
+        </Row>
     )
 }
