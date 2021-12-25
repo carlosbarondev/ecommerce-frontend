d0ce4d60765129bdd329c2858ae4e254e3071e94
carlosbarondev <barondev@gmail.com>
2021-12-25 05:55:43 +0100

    Página del carrito de compras añadida

--- src/components/main/MainScreen.js
+++ src/components/main/MainScreen.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import { Products } from '../products/Products';
 import { ProductScreen } from '../products/ProductScreen';
 import { TopBar } from '../ui/TopBar';
+import { CartScreen } from '../products/CartScreen';
 
 
 export const MainScreen = () => {
@@ -18,6 +19,8 @@ export const MainScreen = () => {
                     <Route path="productos/:ProductoId" element={<ProductScreen />} />
 
                     <Route path="/" element={<Products />} />
+
+                    <Route path="/carrito" element={<CartScreen />} />
 
                 </Routes>
 
--- src/components/ui/Canvas.js
+++ src/components/ui/Canvas.js
@@ -1,12 +1,19 @@
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Button, Offcanvas } from "react-bootstrap";
 
 
 export const Canvas = ({ show, setShow }) => {
 
+    const navigate = useNavigate();
     const { carrito } = useSelector(state => state.cart);
 
     const handleClose = () => setShow(false);
+
+    const handleClick = () => {
+        navigate("/carrito");
+        handleClose();
+    }
 
     return (
         <Offcanvas show={show} onHide={handleClose} placement={'end'}>
@@ -19,7 +26,7 @@ export const Canvas = ({ show, setShow }) => {
                         ?
                         <div className="mb-4 d-grid gap-2">
                             <h3>TOTAL: {carrito.reduce((n, { unidades, producto }) => n + unidades * producto.precio, 0)} €</h3>
-                            <Button className="mt-1" variant="warning" size="lg" onClick={handleClose}>
+                            <Button className="mt-1" variant="warning" size="lg" onClick={handleClick}>
                                 Realizar Pedido
                             </Button>
                             <hr />
@@ -42,7 +49,7 @@ export const Canvas = ({ show, setShow }) => {
                                     <h3>{cart.producto.nombre}</h3>
                                     <ul className="list-group list-group-flush">
                                         <li className="list-group-item"><b>Unidades: </b>{cart.unidades}</li>
-                                        <li className="list-group-item"><b>Precio: </b>{cart.unidades * cart.producto.precio} €</li>
+                                        <li className="list-group-item"><b>Precio: </b>{(cart.producto.precio * cart.unidades).toFixed(2)} €</li>
                                     </ul>
                                 </div>
                                 <hr />
